41eea2bc15bb23e0d8c2af48510860e0a3aaba71
lscamargo <95lucascamargo@gmail.com>
2019-12-15 15:58:19 -0300

    Selecao de roi completa

--- webcam.py
+++ webcam.py
@@ -77,46 +77,43 @@ class camClass:
 		return ret, img
 	
 class uiClass :
-	global mouseX, mouseY, mouseFlag
-	def __init__(self):
-		cv2.namedWindow("Selecione a Região de Interesse", 0)
+	def select_roi (self):
+		global mouseX, mouseY, mouseFlag
+		print ("Mouse click detectado")
+		mouseX, mouseY, mouseFlag = -1, -1, 0
+		cv2.namedWindow("Selecione a Região de Interesse")
 		cv2.setMouseCallback("Selecione a Região de Interesse", get_mouse_position)
-	def selecionar_roi (self):
-		#initialize mouse callback variables
-		mouseFlag = 0
-		mouseX = 0
-		mouseY = 0
-		#initialize return variables
-		ret = 0
-		x = 0
-		y = 0
-		while(True):
-			ret, img = cam.capturar()
-			#sucessful read
-			if (ret == True):
-				cv2.imshow("Selecione a Região de Interesse", img)
-				if(cv2.waitKey(1) & 0xFF == ord('q')):
-					print ("Q button pressed.")
-					ret = 1
-					break
-			#read problem
-			else:
-				print("Could not capture image.")
-				ret = 2
-				break
-			#mouse clicked event
-			if(mouseFlag):
-				print('Mouse FLAG detected position', mouseX,mouseY)
-				x,y = mouseX,mouseY
-				break
-		return ret, x, y, img
+		ret, img = cam.capturar()
+		if (ret == 0) :
+			print ("Falha o capturar")
+			return -1, -1, -1
+		cv2.imshow("Selecione a Região de Interesse", img)
+		
+		print ("Clique na imagem para selecionar uma estrela")
+
+		print("mouseFlag =", mouseFlag)
+
+		while( True) :
+			key = cv2.waitKey(1) & 0xFF 
+			if (key == ord('q')) :
+				print ("Q button pressed.")
+				sys.exit(0)
+			if (key == ord('\n')) :
+				print ("Enter button pressed.")
+				print("mouseFlag =", mouseFlag)
+
+			if (mouseFlag == 1) :
+				print ("Mouse click detectado")
+				return 0, mouseX, mouseY
+				
 
 def get_mouse_position(event,x,y,flags,param):
 	global mouseX, mouseY, mouseFlag
 	if event == cv2.EVENT_LBUTTONUP:
-		print('Mouse flag detected click', x, y)
+		print('Mouse flag detected click', x, y, mouseFlag)
 		mouseX, mouseY = x,y
 		mouseFlag = 1
+		print("mouseFlag =", mouseFlag)
 
 #info fisica
 d = 1.51e-3         #pupil size = 1.51 mm
@@ -126,6 +123,9 @@ pixsize = 3.75e-6       #Pixel size
 
 print ('d= ', d, "l= ", l, "f= ", f)
 
+#mouse global variables
+mouseX, mouseY, mouseFlag = -1, -1, 0
+
 # truncating the image's boundaries to its limits. Makes possible the
 # cropping
 cam = camClass()
@@ -134,7 +134,7 @@ ui = uiClass()
 #cv2.imshow("imagem capturada", img)
 #cv2.waitKey(0)
 #cv2.destroyWindow("imagem capturada")
-ret, x, y, img = ui.selecionar_roi()
+ret, x, y = ui.select_roi()
 if (ret > 0):
 	print("Closing application")
 	sys.exit(0)
@@ -149,33 +149,33 @@ if (x>cam.xmax-50) :
 elif x<50 :
    x=50
 
-# Convert RGB to grayscale.
-grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
-# Crops the image
-grayImageCrop = grayImage[(y-49):(y+49)][(x-49):(49+x)];
-
-# TCOG calculus over the cropped image
-[slopey, slopex] = TCOG(double(grayImageCrop),0.8);    #Thresholded center of gravity
-
-#circle coordinates
-y_circle=slopey+50;
-x_circle=slopex+50;
-
-# Display the image.
-cam.imshow("output", grayImageCrop);
-plot(x_circle, y_circle, 'ro', 'MarkerSize', 10);
-
-#gera os comandos
-if slopey>5 :
-    cima()
-elif slopey<-5 :
-    baixo
-else:
-    parado()
-
-if slopex<-5 :
-    direita()
-elif slopex>5 :
-    esquerda()
-else:
-    parado()
+## Convert RGB to grayscale.
+#grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
+## Crops the image
+#grayImageCrop = grayImage[(y-49):(y+49)][(x-49):(49+x)];
+#
+## TCOG calculus over the cropped image
+#[slopey, slopex] = TCOG(double(grayImageCrop),0.8);    #Thresholded center of gravity
+#
+##circle coordinates
+#y_circle=slopey+50;
+#x_circle=slopex+50;
+#
+## Display the image.
+#cam.imshow("output", grayImageCrop);
+#plot(x_circle, y_circle, 'ro', 'MarkerSize', 10);
+#
+##gera os comandos
+#if slopey>5 :
+#    cima()
+#elif slopey<-5 :
+#    baixo
+#else:
+#    parado()
+#
+#if slopex<-5 :
+#    direita()
+#elif slopex>5 :
+#    esquerda()
+#else:
+#    parado()
