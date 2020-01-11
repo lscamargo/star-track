94c040dd2d0c2390494005a0621807fb3f9eb7ea
Lucas da Silva Camargo <95lucascamargo@gmail.com>
2020-01-11 17:20:41 -0300

    Criada função que calcula o centro de gravidade

--- processamento_de_imagem.py
+++ processamento_de_imagem.py
@@ -8,8 +8,23 @@ def show(img, name = "name"):
 def print_size(img):
 	print("Width:", len(img))
 	print("Height:", len(img[0]))
-	#print("Colors:", len(img[0][0]))
 
+def centro_gravidade(grayImg, threshold = 127):
+	print (grayImg)
+	width = len(grayImg)
+	height = len(grayImg[0])
+	thImg = (grayImg >= threshold) * grayImg
+
+	xcenter, ycenter, w, wTotal = 0, 0, 0, 0
+	for x in range(0, width-1):
+		for y in range(0, height-1):
+			w = thImg[x][y]
+			wTotal += w
+			xcenter += x*w
+			ycenter += y*w
+	xcenter /= wTotal
+	ycenter /= wTotal
+	return xcenter, ycenter
 
 import cv2
 width = 640
@@ -28,3 +43,6 @@ print_size(img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 show(gray, "Escala de cinza")
 print_size(gray)
+
+x,y = centro_gravidade(gray)
+print(x,y)
