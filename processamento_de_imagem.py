49f2c1bcd78239867721cfdb7c3494796738ddc5
Lucas da Silva Camargo <95lucascamargo@gmail.com>
2020-01-15 20:05:12 -0300

    teste do centro de gravidade

--- processamento_de_imagem.py
+++ processamento_de_imagem.py
@@ -3,25 +3,26 @@ import numpy as np
 
 def show(img, name = "name"):
 	cv2.imshow(name, img)
-	cv2.waitKey()
+	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-def print_size(img):
-	print("Width:", len(img))
-	print("Height:", len(img[0]))
-
 def centro_gravidade(grayImg, threshold = 127):
-	width = len(grayImg)
-	height = len(grayImg[0])
+	height, width = grayImg.shape
+	#print("width = ", width)
+	#print("height = ", height)
+	#thImg = np.zeros((width, height))
 	thImg = (grayImg >= threshold) * grayImg
-
+	cv2.waitKey(0)
 	xcenter, ycenter, w, wTotal = 0, 0, 0, 0
 	for x in range(0, width-1):
 		for y in range(0, height-1):
-			w = thImg[x][y]
+			w = thImg[y,x]
+			#print("w = ", w)
 			wTotal += w
+			#print("wTotal = ", wTotal)
 			xcenter += x*w
 			ycenter += y*w
+	print("wTotal = ", wTotal)
 	xcenter /= wTotal
 	ycenter /= wTotal
 	return xcenter, ycenter
@@ -33,19 +34,28 @@ exposure_time = 0.1
 
 cap = cv2.VideoCapture(0)
 
-w, h = 3, 3;
-Matrix = np.matrix([[0 for x in range(w)] for y in range(h)])
-Matrix[0][0]=1
-print(Matrix)
-x,y = centro_gravidade(Matrix, 0)
-print(x,y)
+#w, h = 3, 5;
+#Matrix = np.matrix([[0 for x in range(w)] for y in range(h)])
+#Matrix[0,0] = 2
+#Matrix[0,1] = 2
+#Matrix[0,2] = 2
+#Matrix[1,0] = 2
+#Matrix[2,0] = 2
+#print(Matrix)
+#x,y = centro_gravidade(Matrix, 0)
+#print(x,y)
 
-while (False):
+ret, img = cap.read()
+show(img)
+
+while (True):
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	cv2.imshow("Escala de cinza", gray)
-	x,y = centro_gravidade(gray)
+	cutGray = gray[0:100,0:70]
+	cutGray[00:10,50:70] = np.ones((10,20)) * 255
+	x,y = centro_gravidade(cutGray)
+	cv2.imshow("recorte", cutGray)
+	key = cv2.waitKey(1) & 0xFF
 	print(x,y)
-	key = cv2.waitKey(1) & 0xFF 
-	if (key == ord('q')) :
+	if(key == ord('q')):
 		break
